ef5657284c0e865b8c3936436cc40c467c545f06
David <drs@a.recife.ifpe.edu.br>
2023-01-03 23:58:32 -0300

    [feat] add better alg

--- BitParity.cc
+++ BitParity.cc
@@ -6,6 +6,8 @@
 
 /**
  * iteratively test each bit.
+ * 
+ * time complexity O(n)
 */
 short simpleParity(unsigned long long mask) {
     short response = 0;
@@ -16,12 +18,31 @@ short simpleParity(unsigned long long mask) {
     return response;
 }
 
+/**
+ * this approach uses a trick to remove the last set of 1 bit and this implies
+ * to most efficient executions given that we don't need to shift right in every
+ *  bit to test if its a '1'.
+ * 
+ * time complexity O(k) where k is the number of 1s in bit word.
+*/
+short betterParity(unsigned long long mask) {
+    short resp = 0;
+
+    while (mask) {
+        resp = resp ^ 1;
+        mask = mask & (mask -1); // trick
+    }
+    
+    return resp;
+}
+
+
 
 int main(void) {
 
 
     assert(simpleParity(11) == 1);
-    //std::cout << "Response: " << simpleParity(11) << std::endl;
+    assert(betterParity(11) == 1);
 
     return 0;
 }
